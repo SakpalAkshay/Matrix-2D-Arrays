d9a4ac91775803243437022df859aafd1775f6f7
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-10-29 18:20:08 -0700

    Updated 01_displaying_matrix.py

--- 01_displaying_matrix.py
+++ 01_displaying_matrix.py
@@ -1,1 +1,13 @@
+#program to display matrix elements
+
+
+arr = [[1,2,3,], [9,87,6,23], [0,1], [1,2,3,4,5,6,7]]
+
+
+#using for loop 
+for i in range(len(arr)):
+    for j in range(len(arr[i])):
+        print(arr[i][j], end=" ")
+    print()
+
 
