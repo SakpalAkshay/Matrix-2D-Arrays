637f800e4e9105eac708c8eb8d9212a8192abe00
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-11-01 14:31:48 -0700

    Update 03_Zig-Zag_Traversal.py

--- 03_Zig-Zag_Traversal.py
+++ 03_Zig-Zag_Traversal.py
@@ -1,1 +1,13 @@
 
+n, m = map(int, input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+
+for i in range(n):
+    
+    if i%2 == 0:
+        for j in range(m):
+            print(matrix[i][j], end = " ")
+    else:
+        print(*reversed(matrix[i]), end=" ")
